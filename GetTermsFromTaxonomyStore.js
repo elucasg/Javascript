72bfca85660ee90146fc329ba0e2570d9ad0bc36
elucasg <elucasg@hotmail.com>
2017-08-21 10:25:10 +0200

    Create GetTermsFromTaxonomyStore.js

---
+++ GetTermsFromTaxonomyStore.js
@@ -0,0 +1,22 @@
+function GetTermsFromTaxonomyStore() {
+
+    var context = SP.ClientContext.get_current();
+    var taxSession = SP.Taxonomy.TaxonomySession.getTaxonomySession(context);
+    var termStore = taxSession.getDefaultSiteCollectionTermStore();
+
+    var termsetName = "Owners";
+    var termSets = termStore.getTermSetsByName(termsetName, 1033);
+    var termSet = termSets.getByName(termsetName);
+    var terms = termSet.getAllTerms();
+
+    context.load(terms);
+    context.executeQueryAsync(function () {
+        var termEnumerator = terms.getEnumerator();
+        while (termEnumerator.moveNext()) {
+            var currentTerm = termEnumerator.get_current();
+            var termName = currentTerm.get_name();
+        }
+    }, function (sender, args) {
+        console.log(args.get_message());
+    });
+}
